2997e2f4a70845b748bc61d0c0755b43ad6865d1
Huzhangjie <1534634433@qq.com>
2018-01-24 20:34:20 +0800

    4个大的页面 书城 书架 分类 订阅

---
+++ vue-reader/src/pages/bookstore.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>shucheng</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-reader/src/pages/subscribe.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>dingyue</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
