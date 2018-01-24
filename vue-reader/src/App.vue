b3b047691359c9f363463f30274b26c47f91f890
Huzhangjie <1534634433@qq.com>
2018-01-24 20:36:59 +0800

    header和tabBar的注册 iconfont的引入

--- vue-reader/src/App.vue
+++ vue-reader/src/App.vue
@@ -2,23 +2,30 @@
   <div id="app">
     <read-header />
     <router-view/>
-    <read-tab />
+    <!-- <read-tab /> -->
+    <i class="iconfont icon-fenlei"></i>
   </div>
 </template>
 
 <script>
+import readHeader from '@/components/ReaderHeader'
+import rearTab from '@/components/ReaderTab'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    readHeader,
+    // readTab
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url(//at.alicdn.com/t/font_553259_z4dwb9ehvznqm2t9.css);
+
+* {
+  margin: 0;
+  padding: 0;
 }
+
 </style>
