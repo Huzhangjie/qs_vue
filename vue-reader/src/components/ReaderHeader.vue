0ab6e7cafe9b6db13b85745202cf2ee01ec4d1af
Huzhangjie <1534634433@qq.com>
2018-01-24 20:42:54 +0800

    带有头像的最顶端的组件 每个页面都有 fixed在在最顶端 有一个等高的div占位

---
+++ vue-reader/src/components/ReaderHeader.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+ <div class="wrapper">
+    <img :src="src">
+  </div>
+  <div class="header-block"></div>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      src: "http://s0.hao123img.com/res/img/moe/0922zt.jpg"
+    }
+  }
+}
+</script>
+<style scoped>
+.wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 6vh;
+  background-color: pink;
+  z-index: 10;
+}
+.wrapper img {
+  height: 100%;
+  width: 6vh;
+  background-color: green;
+  float: left;
+  margin: auto 5vw;
+  border-radius: 50%;
+}
+.header-block {
+  height: 6vh;
+  background-color: red;
+}
+</style>
