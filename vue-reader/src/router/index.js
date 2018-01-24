68bfb91a2bbed5830bcd8a60ee9c9ca6f040cb01
Huzhangjie <1534634433@qq.com>
2018-01-24 20:35:17 +0800

    在路由中配置4个页面的路由

--- vue-reader/src/router/index.js
+++ vue-reader/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import bookStore from '@/pages/bookstore'
+import bookShelf from '@/pages/bookshelf'
+import classify from '@/pages/classify'
+import subscribe from '@/pages/subscribe'
 
 Vue.use(Router)
 
@@ -10,6 +14,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/bookStore',
+      component: bookStore
+    },
+    {
+      path: '/bookShelf',
+      component: bookShelf
+    },
+    {
+      path: '/classify',
+      component: classify
+    },
+    {
+      path: '/subscribe',
+      component: subscribe
     }
   ]
 })
