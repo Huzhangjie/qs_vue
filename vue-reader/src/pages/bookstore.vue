b9e8f280a681fbdd0b96dcdd788e9928a8abf873
Huzhangjie <1534634433@qq.com>
2018-01-25 23:32:21 +0800

    注册添加headerTab的组件

--- vue-reader/src/pages/bookstore.vue
+++ vue-reader/src/pages/bookstore.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>shucheng</div>
+  <div>
+    <header-tab />
+  </div>
 </template>
 
 <script>
+import headerTab from '@/components/HeaderTab'
 export default {
-
+  components: {
+    headerTab
+  }
 }
 </script>
 
