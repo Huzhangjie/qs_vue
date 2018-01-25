aa28a5c63ba07f22c582d7d5e75d10956f0f5a51
Huzhangjie <1534634433@qq.com>
2018-01-25 23:35:38 +0800

    使用router-link 切换4个页面的路由

--- vue-reader/src/components/ReaderTab.vue
+++ vue-reader/src/components/ReaderTab.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="wrapper">
     <div class="tab-item" v-for="tab in tabList" :key="tab.name">
-      <i class="iconfont" :class="tab.icon"></i>
-      <div class="name">{{tab.name}}</div>
+      <router-link tag="li" :to="'/' + tab.path">
+        <div class="item-wrapper">
+          <i class="iconfont" :class="tab.icon"></i>
+          <div class="name">{{tab.name}}</div>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -12,18 +16,22 @@
       return {
         tabList: [
           {
+            path: "bookstore",
             name: "书城",
             icon: "icon-shuji"
           },
           {
+            path: "classify",
             name: "分类",
             icon: "icon-fenlei"
           },
           {
+            path: "bookshelf",
             name: "书架",
             icon: "icon-shujia"
           },
           {
+            path: "subscribe",
             name: "订阅",
             icon: "icon-icon--"
           }
@@ -32,31 +40,41 @@
     }
   }
 </script>
-<style>
+<style scoped>
 .wrapper {
+  font-size: 20px;
   display: flex;
   position: fixed;
   bottom: 0;
   width: 100vw;
-  height: 8vh;
+  height: 10vh;
+  background-color: pink;
+  z-index: 10;
 }
 .wrapper:before {
   content: '';
   position: absolute;
-  top: 0;
+  top: 0px;
   left: 0;
   right: 0;
   border-top: 1px solid red;
-  transform: scaleY(.5)
+  transform: scaleY(.2)
 }
 .tab-item {
   flex: 1;
+}
+.item-wrapper {
+  width: 25vw;
+  height: 10vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 }
 .iconfont {
-  font-size: 24px;
+  font-size: 26px;
+}
+.router-link-active {
+  color: red;
 }
 </style>
