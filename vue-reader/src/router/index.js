4ae20e277c4bffe360d15170f367286d2a3059cf
Huzhangjie <1534634433@qq.com>
2018-01-28 00:05:46 +0800

    向bookstore中添加子路由

--- vue-reader/src/router/index.js
+++ vue-reader/src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import bookStore from '@/pages/bookstore'
 import bookShelf from '@/pages/bookshelf'
 import classify from '@/pages/classify'
 import subscribe from '@/pages/subscribe'
+import TuiJian from '@/components/bookstore/TuiJian'
+import KouWei from '@/components/bookstore/KouWei'
+import MianFei from '@/components/bookstore/MianFei'
+import PaiHang from '@/components/bookstore/PaiHang'
+import ZhuanTi from '@/components/bookstore/ZhuanTi'
 
 Vue.use(Router)
 
@@ -12,23 +16,52 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/bookStore'
     },
     {
       path: '/bookStore',
-      component: bookStore
+      name: '书城',
+      component: bookStore,
+      children: [{
+          path: 'tuijian',
+          name: '推荐',
+          component: TuiJian
+        },
+        {
+          path: 'kouwei',
+          name: '口味',
+          component: KouWei
+        },
+        {
+          path: 'mianfei',
+          name: '免费',
+          component: MianFei
+        },
+        {
+          path: 'paihang',
+          name: '排行',
+          component: PaiHang
+        },
+        {
+          path: 'zhuanti',
+          name: '专题',
+          component: ZhuanTi
+        }
+      ]
     },
     {
       path: '/bookShelf',
+      name: '书架',
       component: bookShelf
     },
     {
       path: '/classify',
+      name: '分类',
       component: classify
     },
     {
       path: '/subscribe',
+      name: '订阅',
       component: subscribe
     }
   ]
