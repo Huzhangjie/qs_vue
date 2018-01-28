666c911f9ced1b0ff857c528a1ad512984fdda8d
Huzhangjie <1534634433@qq.com>
2018-01-28 21:12:06 +0800

    推荐页单本书籍的组件

---
+++ vue-reader/src/components/BookItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="book-item">
+    <img class="book-item_cover" :src="book.image" mode="aspectFit">
+    <div class="book-item_title">{{book.title}}</div>
+    <div class="book-item_author">{{book.author}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['book']
+}
+</script>
+
+<style scoped>
+.book-item {
+  overflow: hidden;
+  margin: 0 3.3333vw;
+  margin-bottom: 3vh;
+}
+.book-item_cover {
+  width: 26.6385vw;
+}
+.book-item_title {
+  width: 26.6385vw;
+  color: black;
+}
+.book-item_author {
+  color: #888888;
+  font-size: 12px;
+}
+</style>
