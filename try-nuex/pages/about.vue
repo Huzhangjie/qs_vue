16b312935198c4f1611ccc0e43ac67695f9cdae4
Huzhangjie <1534634433@qq.com>
2018-03-10 23:25:09 +0800

    全局变量process访问，不必使用require命令加载 返回系统信息

---
+++ try-nuex/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Hi from {{name}}</p>
+    <nuxt-link to="/">Back Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    // nuxt本质 将.vue 交给后端渲染
+    // webpack server-side-renderer
+    // react next.js
+    // .vue => html css js
+    return {
+      name: process.static ? 'static' : (process.server ? 'server' : 'client')
+    }
+  },
+  head: {
+    title: 'About Page'
+  }
+}
+</script>
+
+<style>
+
+</style>
