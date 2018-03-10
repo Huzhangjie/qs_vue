e7a1e6d0da77babdcfaf1292f3bf53de8868eb5a
Huzhangjie <1534634433@qq.com>
2018-03-10 23:21:47 +0800

    根据不同环境判断user-agent的值 再渲染至页面上

---
+++ try-nuex/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>User Agent</h1>
+    <p>{{ userAgent }}</p>
+    <p>
+      <nuxt-link to="posts">Blog</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    asyncData: ({ req }) => ({
+      userAgent: (req ? req.headers['user-agent'] :
+        (typeof navigator !== undefined ? navigator.userAgent : 'No user agent(agenerate)'))
+    })
+  }
+</script>
+
+<style>
+
+</style>
