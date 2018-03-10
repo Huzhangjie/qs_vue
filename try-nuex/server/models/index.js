d8240667a22bf6ecef8510ada3278f240d4f6267
Huzhangjie <1534634433@qq.com>
2018-03-10 23:36:24 +0800

    连接数据库 寻找是否有对应的user 没有的话就添加一条config中设置好的user

---
+++ try-nuex/server/models/index.js
@@ -0,0 +1,21 @@
+// mongoose 官方推荐的mongodb的驱动 数据库抽象
+import mongoose from 'mongoose'
+import config from '../config'
+require('./user')
+const User = mongoose.model('User')
+
+const mongoUrl = `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.database}`
+mongoose.Promise = global.Promise
+mongoose.connection.openUri(mongoUrl).once('open', async () => {
+  console.log('database connect successed')
+  let userInfo = config.user
+  let user = await User.findOne({ 
+    role: 'superAdmin'}).exec()
+  if(!user) {
+    user = new User(userInfo)
+    await user.save()
+    console.log('管理员已生成')
+  }
+}).on('error', (error) => {
+  console.log(error)
+})
