0b10430a5348e4c667506294733aa15ebf012b52
Huzhangjie <1534634433@qq.com>
2018-03-12 23:16:15 +0800

    路由映射动态传参 :page?和:limit?当前页和第几条数据

--- try-nuex/server/routes/index.js
+++ try-nuex/server/routes/index.js
@@ -3,11 +3,13 @@ import config from '../config'
 import db from '../models'
 
 const user = require('../controllers/user')
+const article = require('../controllers/article')
 const router = new Router({
   prefix: config.app.routerBaseApi
 })
 // url /api/user => router 匹配 => controller 后端处理业务逻辑
 // => model view 通信
 router.get('/user', user.getUserInfo)
+  .get('/articles/:page?/:limit?', article.getArticles)
 
 export default router
