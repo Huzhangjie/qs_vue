ad1e23a6dcc864a6fa0b854425af92e306e2b4de
Huzhangjie <1534634433@qq.com>
2018-03-16 23:20:55 +0800

    从npm库中引用vue-drawer-layout 抽屉式菜单 使用其api添加点击遮罩层收起菜单事件

--- vue-reader/src/App.vue
+++ vue-reader/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <read-header />
-    <router-view/>
-    <read-tab />
-  </div>
+  <vue-drawer-layout ref="drawer" @mask-click="closeDrawer">
+    <div class="drawer-content" slot="drawer">
+      <slide-menu></slide-menu>
+    </div>
+    <div slot="content"> 
+      <read-header @drawer="drawer"/>      
+      <router-view />
+      <read-tab />
+    </div>
+  </vue-drawer-layout>
 </template>
 
 <script>
+import slideMenu from '@/components/SlideMenu'
 import readHeader from '@/components/ReaderHeader'
 import readTab from '@/components/ReaderTab'
 
@@ -14,13 +20,27 @@ export default {
   name: 'App',
   components: {
     readHeader,
-    readTab
+    readTab,
+    slideMenu
+  },
+  mounted() {
+    // console.log(this.$refs)
+    // this.$refs.drawer.toggle(true)
+  },
+  methods: {
+    drawer() {
+      // 子组件emit方法 点击头像打开菜单
+      this.$refs.drawer.toggle(true)
+    },
+    closeDrawer() {     
+      this.$refs.drawer.toggle(false)
+    }
   }
 }
 </script>
 
 <style>
-@import url(//at.alicdn.com/t/font_553259_tcpf7vtr9zilik9.css);
+@import url(//at.alicdn.com/t/font_553259_eqyo7kxkcphf47vi.css);
 
 * {
   margin: 0;
@@ -29,5 +49,6 @@ export default {
 li {
   list-style: none;
 }
+
 
 </style>
