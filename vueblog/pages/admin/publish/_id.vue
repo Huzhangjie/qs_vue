1f1352cad2404d1c9a0f8ea8586a29fb70bdb664
Huzhangjie <1534634433@qq.com>
2018-03-17 22:59:00 +0800

    nuxt框架会根据pages文件夹下的文件目录和文件名自动生成对应的路由 _id.vue引入TopEditor组件

---
+++ vueblog/pages/admin/publish/_id.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="admin-publish container">
+      <div class="publish-title">
+          <input type="text" placeholder="文章标题" v-model="article.title">
+      </div>
+      <div class="publish-content">
+          <top-editor v-model="article.content" 
+            :upload="upload" :options="options" @save="save">
+          </top-editor>
+      </div>
+      <div class="publish-handle">
+          <input type="text" placeholder="回车可创建新标签" v-model="tag">
+          <div class="handle-button">
+              <button @click="publish(false)">存草稿箱</button>
+              <button @click="publish(true)">发布</button>
+          </div>
+      </div>
+      <Tip ref="tip"></Tip>
+  </div>
+ </template>
+<script>
+import TopEditor from 'top-editor/src/lib/TopEditor.vue'
+export default {
+  data() {
+      return {
+          upload: {
+          },
+          options: {},
+          article: {
+              title: '',
+              content: '',
+              tags: []
+          },
+          tag: '',
+          tags: []
+      }
+  },
+  methods: {
+    save() {},
+      publish(isPublish) {
+        //   存到mongodb中，是草稿还是publish
+        if(!this.article.title || !this.article.content) {
+          this.$refs.tip.openTip('标题和内容不能为空！')
+          // console.log('填写不完整')
+        } else {
+          let article = {
+            title: this.article.title,
+            content: this.article.content
+          }
+          this.$store.dispatch('CREATE_ARTICLE', article).then(data => {
+            // console.log(data)
+            if(data.success) {
+                this.$refs.tip.openTip('文章创建完成')
+                this.article.title = ''
+                this.article.content = ''
+            }
+          })
+        }
+      }
+  },
+  components: {
+    TopEditor
+  }
+}
+</script>
+<style>
+
+</style>
