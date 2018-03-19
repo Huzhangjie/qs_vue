411ede1e35356a77f904a7b88ebb4469a38bad80
Huzhangjie <1534634433@qq.com>
2018-03-19 22:10:45 +0800

    添加了getArticles等方法

--- vueblog/server/controllers/article.js
+++ vueblog/server/controllers/article.js
@@ -31,3 +31,64 @@ export const postArticle = async (ctx, next) => {
   }
 
 }
+
+export const getArticles = async (ctx, next) => {
+  let {page = 1, limit = 15} = ctx.params
+  page = Number((page-1) * limit) || 0
+  limit = Number(limit) || 15
+
+  try{
+    let total = await Article.find({ publish: true }).exec()
+    total = total.length
+    const data = await Article.find({ publish: true })
+      .populate({
+        path: 'tags',
+        select: 'id name'
+      })
+      .skip(page)
+      .limit(limit)
+      .sort({'createdAt': -1})
+      .exec()
+    ctx.body = {
+      success: true,
+      data: data,
+      total: total
+    }
+  }catch(e) {
+    ctx.body = {
+      success: false,
+      err: e,
+      totle: 0
+    }
+  }
+}
+
+export const getArticle = async(ctx, next) => {
+  let { id } = ctx.params
+  if (!id) {
+    return (ctx.body = {
+      success: false,
+      err: 'id is required'
+    })
+  }
+
+  try {
+    let article = await Article.findOne({ _id: id })
+      .populate({
+        path: 'tags',
+        select: 'id name'
+      })
+      .exec()
+      await Article.findByIdAndUpdate(id, {views: article.views + 1}).exec()
+    ctx.body = {
+      success: true,
+      data: article
+    }
+  } catch (e) {
+    // console.log(e)
+    ctx.body = {
+      success: false,
+      err: e
+    }
+  }
+}
