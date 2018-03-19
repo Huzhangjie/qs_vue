3b63d8c2f0824edd1c6bff682d777d08936e4dbc
Huzhangjie <1534634433@qq.com>
2018-03-19 22:01:04 +0800

    添加post /login的路由 和 post get /article的路由

--- vueblog/server/routes/index.js
+++ vueblog/server/routes/index.js
@@ -1,11 +1,17 @@
 import Router from 'koa-router'
 import db from '../models'
 import config from '../config'
+
 const article = require('../controllers/article')
+const user = require('../controllers/user')
 const router = new Router({
   prefix: config.app.routerBaseApi
 })
 
+router.post('/login', user.login)
+
 router.post('/article', article.postArticle)
+      .get('/article/:id?', article.getArticle)
+      .get('/articles/:page?/:limit?', article.getArticles)
 
 export default router
