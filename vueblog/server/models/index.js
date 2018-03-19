f879f1db677ba57a56f761dfafa9647866237e73
Huzhangjie <1534634433@qq.com>
2018-03-19 22:05:26 +0800

    引入入user的model 连接数据库判断有无superAdmin 无则添加进数据库

--- vueblog/server/models/index.js
+++ vueblog/server/models/index.js
@@ -1,7 +1,43 @@
+// import mongoose from 'mongoose'
+// import config from '../config'
+
+// require('./article')
+// require('./user')
+
+// const User = mongoose.model('User')
+
+// const mongoUrl = `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.database}`
+// mongoose.Promise = global.Promise
+// mongoose.connection
+//   .openUri(mongoUrl)
+//   .once('open', async () => {
+//     console.log('database connect successed')
+//     // init admin information
+//     let userInfo = config.user
+//     // console.log(userInfo)
+//     userInfo.password = md5(userInfo.password)
+
+//     let user = await User.findOne({ role: 'superAdmin' }).exec()
+//     console.log(user + "user")
+//     if (!user) {
+//       user = new User(userInfo)
+//       await user.save()
+//       console.log('Administrator information initialization succeeded')
+//     }
+    
+//   })
+//   .on('error', (error) => {
+//     console.warn('database connect fail', error)
+//   })
 import mongoose from 'mongoose'
+import md5 from 'md5'
 import config from '../config'
 
+require('./user')
+// require('./tag')
 require('./article')
+
+const User = mongoose.model('User')
 
 const mongoUrl = `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.database}`
 mongoose.Promise = global.Promise
@@ -10,6 +46,16 @@ mongoose.connection
   .once('open', async () => {
     console.log('database connect successed')
     // init admin information
+    let userInfo = config.user
+    userInfo.password = md5(userInfo.password)
+
+    let user = await User.findOne({ role: 'superAdmin' }).exec()
+    console.log(user)
+    if (!user) {
+      user = new User(userInfo)
+      await user.save()
+      console.log('Administrator information initialization succeeded')
+    }
     
   })
   .on('error', (error) => {
