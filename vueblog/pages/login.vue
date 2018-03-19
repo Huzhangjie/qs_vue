f0cf9281744cdfe50ff8ce11fd8cbd9340044ad6
Huzhangjie <1534634433@qq.com>
2018-03-19 21:51:47 +0800

    用户写新随笔时判断是否登录 未登录则先跳转至login界面

---
+++ vueblog/pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login container">
+    <input type="text" class="login-username" placeholder="用户名" autocomplete="off" v-model="user.username">
+    <input type="password" class="login-password" placeholder="密码" autocomplete="off" v-model="user.password" @keyup.enter="login">
+    <button class="login-button" @click="login">登 录</button>
+    <Tip ref="tip"></Tip>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user:{}
+    }
+  },
+  head () {
+    return {
+      title: '登录 - VueBlog'
+    }
+  },
+  methods: {
+    login () {
+      if(!this.user.username || !this.user.password) {
+        console.log("no data")
+        return
+      }
+      this.$store.dispatch('LOGIN', this.user).then(data => {
+        if(data.success) {
+          this.$router.push('/admin/publish')
+        } else {
+          this.$refs.tip.openTip('用户名或密码不正确')
+        }
+      })
+    }
+  }
+}
+</script>
